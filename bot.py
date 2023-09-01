0262b13a5bc6afc64106b91cda4e9b948d77707e
LERINA DEPI PERMANA <92065759+chantat21@users.noreply.github.com>
2023-09-02 01:03:28 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -65,7 +65,7 @@ class Bot(Client):
                     f"Silakan periksa kembali var FORCE_SUB_CHANNEL2 dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Group Saat Ini: {FORCE_SUB_CHANNEL2}"
                 )
                 self.LOGGER(__name__).info(
-                    "\nBot Berhenti. Gabung Group https://t.me/pantekyks untuk Bantuan"
+                    "\nBot Berhenti. Gabung Group https://t.me/suhersupport untuk Bantuan"
                 )
                 sys.exit()
         try:
@@ -79,13 +79,13 @@ class Bot(Client):
                 f"Pastikan Bot adalah Admin di Channel DataBase, dan Periksa kembali Nilai CHANNEL_ID, Nilai Saat Ini: {CHANNEL_ID}"
             )
             self.LOGGER(__name__).info(
-                "\nBot Berhenti. Gabung Group https://t.me/pantekyks untuk Bantuan"
+                "\nBot Berhenti. Gabung Group https://t.me/suhersupport untuk Bantuan"
             )
             sys.exit()
 
         self.set_parse_mode("html")
         self.LOGGER(__name__).info(
-            f"[🔥 BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan ke https://t.me/mahadappa"
+            f"[KONTOL🔥BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan ke https://t.me/mahadappa"
         )
         self.username = usr_bot_me.username
 
